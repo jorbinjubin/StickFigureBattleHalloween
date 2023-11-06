32f54fb0446c37c2e5c3ab890b68a55f64620712
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2023-11-05 20:39:41 -0500

    Update MyCreation.java

--- MyCreation.java
+++ MyCreation.java
@@ -1,32 +1,47 @@
+// The "MyCreation" class.
 import hsa.Console;
 import java.awt.*;
 /*
-~~%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%~~
-  * My Creation assignment. Uses threads and loops to animate a short story about 2  
-    stick figures who are wizards and fight each other in a halloween themed setting.
-    The bubbles in the cauldron use an overloaded constructor to create bubbles of   
-    different sizes and speeds.                                                      
-										     
-  * @author Justin Jiang, Fei Wang                                                   
-  * ICS3UP                                                                           
-  * 2023/month/day                                                                   
-~~%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%~~
-  */
+ * Runs the animation by creating objects and running them. Uses threads to run 
+ * multiple animated objects at the same time. Bubbles use an overloaded constructor
+ * to create bubbles of different sizes and speeds.
+ * 
+ * @author Justin Jiang, Fei Wang
+ * ICS3UP
+ * 2023/11/02
+ */
 
 public class MyCreation {
     static Console c; // The output console
 
     // adds the Background thread to MyCreation
     public void background() {
-	// not a thread because it's not animated!!!
-	Background b = new Background(c);
-	b.bg1();
+        // not a thread because it's not animated!!!
+        Background b = new Background(c);
+        b.bg1();
+        b.moon();
+    }
+
+    public void splash() {  
+        Splash s = new Splash(c);
+        s.start();
+        try{s.join();} catch (InterruptedException e){}
     }
     
-    public void splash() {  
-	Splash s = new Splash(c);
-	s.start();
-	try{s.join();} catch (InterruptedException e){}
+    public void spider() {
+        Spider sp = new Spider(c);
+        sp.run();
+        
+    }
+    
+    public void sm1() {
+        Stickman1 h = new Stickman1(c);
+        h.start();
+    }
+    
+    public void bat() {
+        Bat b = new Bat(c);
+        b.run();
     }
     // adds the Lightning thread to MyCreation
     //  public void lightning ()
@@ -47,28 +62,19 @@ public class MyCreation {
     //      }
     //  }
 
-    
-    public void sm1() {
-	Stickman1 h = new Stickman1(c);
-	h.start();
-    }
-    
     // creates a new window and gives window a title
-    public MyCreation() {
-	c = new Console("Stick Figure Battle");
-    }
     
+    public MyCreation() {
+        c = new Console("Stick Figure Battle");
+    }
 
     public static void main(String[] args) {
-	MyCreation z = new MyCreation();
-	
-	z.background();
-	z.splash();
-	z.sm1();
-	
-	
-	
-	
+        MyCreation z = new MyCreation();
+
+        z.background();
+        z.splash();
+        z.sm1();
+        z.spider();
+        z.bat();
     }
 }
-
