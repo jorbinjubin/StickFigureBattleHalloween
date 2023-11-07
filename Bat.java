0ed896d47bfa00bb0b72b2e629dc169d2f209133
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2023-11-07 13:00:10 -0500

    Update Bat.java

--- Bat.java
+++ Bat.java
@@ -13,37 +13,35 @@ public class Bat extends Thread {
     private Console c;
 
     public void drawBat (int x, int y) {      
-        for(int i=0; i<100; i++) {
-            c.setColor(tkit.bat);
+        c.setColor(tkit.bat);
             c.fillOval(100, 60, 20, 20);
-            int [] x1 = {105, 80, 70, 72, 88, 105};
-            int [] y1 = {65, 50, 58, 63, 70, 75};
+            int [] x1 = {x, x-20, x-30, x-28, x-12, x+5};
+            int [] y1 = {y, y-15, y-7, y-2, y+5, y+10};
             c.fillPolygon(x1, y1, 6);
-            int [] x2 = {115, 140, 150, 148, 132, 115};
-            int [] y2 = {65, 50, 58, 63, 70, 75};
+            int [] x2 = {x+15, x+40, x+50, x+48, x+32, x+15};
+            int [] y2 = {y, y-15, y-7, y-2, y+5, y+10};
             c.fillPolygon(x2, y2, 6);
             c.setColor(tkit.orangeBg);
-            c.fillArc(70, 60, 12, 12, -10, 190);
-            c.fillArc(80, 65, 12, 12, -10, 190);
-            c.fillArc(89, 68, 12, 12, -10, 190);
+            c.fillArc(x-30, y-5, 12, 12, -10, 190);
+            c.fillArc(x-20, y, 12, 12, -10, 190);
+            c.fillArc(x-11, y+3, 12, 12, -10, 190);
             
-            c.fillArc(139, 60, 12, 12, 0, 190);
-            c.fillArc(129, 65, 12, 12, -10, 190);
-            c.fillArc(119, 68, 12, 12, -10, 190);
-            c.fillOval(104, 70, 4, 4);
-            c.fillOval(112, 70, 4, 4);
+            c.fillArc(x+39, y-5, 12, 12, 0, 190);
+            c.fillArc(x+29, y, 12, 12, -10, 190);
+            c.fillArc(x+19, y+3, 12, 12, -10, 190);
+            c.fillOval(x+4, y+5, 4, 4);
+            c.fillOval(x+12, y+5, 4, 4);
             
-            c.setColor(tkit.spider);
-            c.drawLine(100, 65, 80, 50);
-            c.drawLine(80, 50, 69, 60);
-            c.drawLine(117, 65, 140, 50);
-            c.drawLine(140, 50, 151, 60);
+            c.setColor(Color.black);
+            c.drawLine(x, y, x-20, y-15);
+            c.drawLine(x-20, y-15, x-31, y-5);
+            c.drawLine(x+17, y, x+40, y-15);
+            c.drawLine(x+40, y-15, x+51, y-5);
             
-            c.drawLine(100, 64, 80, 49);
-            c.drawLine(80, 49, 69, 60);
-            c.drawLine(117, 64, 140, 49);
-            c.drawLine(140, 49, 151, 60);
-        }       
+            c.drawLine(x, y-1, x-20, y-16);
+            c.drawLine(x-20, y-11, x-31, y-5);
+            c.drawLine(x+17, y-1, x+40, y-16);
+            c.drawLine(x+40, y-11, x+51, y-5);     
     }
 
 
@@ -53,6 +51,6 @@ public class Bat extends Thread {
 
 
     public void run () {
-        drawBat(105, 65);
+        drawBat(100, 65);
     }
 }
