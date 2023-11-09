f89c74f908aca5895b9830d85a4a15eba51dc379
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2023-11-08 20:55:39 -0500

    Update Ghost.java

--- Ghost.java
+++ Ghost.java
@@ -15,11 +15,21 @@ public class Ghost extends Thread {
 
     public void drawGhost (int x, int y) {
         c.setColor(Color.white);
-        c.fillRect(x, y, 60, 30);
+        c.fillRect(x, y, 60, 40);
         c.fillArc(x, y-30, 60, 60, 0, 180);
-        c.fillArc(x, y+20, 20, 20, 180, 180);
-        c.fillArc(x+20, y+20, 20, 20, 180, 180);
-        c.fillArc(x+40, y+20, 20, 20, 180, 180);
+        c.fillArc(x-1, y+30, 22, 20, 180, 180);
+        c.fillArc(x+19, y+30, 22, 20, 180, 180);
+        c.fillArc(x+39, y+30, 22, 20, 180, 180);
+        
+        c.setColor(Color.black);
+        c.fillOval(x+12, y-2, 10, 15);
+        c.fillOval(x+40, y-2, 10, 15);
+        
+        //tongue
+        c.setColor(tkit.lightPink);
+        int[] n = {x+23, x+38, x+31, x+29};
+        int[] m = {y+20, y+20, y+25, y+25};
+        c.fillPolygon(n, m, 4);
     }
 
 
