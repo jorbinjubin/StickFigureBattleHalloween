1649eba8e2d1ea6e51ec8186b733be8501fd891d
tszfeiw <fei.wang3@student.tdsb.on.ca>
2023-11-09 19:16:29 -0500

    added tkit colors to bg

--- Background.java
+++ Background.java
@@ -11,83 +11,105 @@ import hsa.Console;
 public class Background {
     //Declaring the local console variable
     private Console c;
-
+    private static Color sun = new Color(208, 66, 14); 
+    private static Color pumpkin = new Color(255, 170, 10); 
+    private static Color pumpkinEyes = new Color(255, 219, 77); 
+    private static Color pumpkinStem = new Color(112, 58, 3);
+    private static Color lightPink = new Color(252, 159, 238);
+    private static Color mouse = new Color(212, 212, 212);
+    private static Color bat = new Color(58, 9, 96);
+    private static Color spider = new Color(74, 0, 0);
+    private static Color broom = new Color(237, 197, 128);
+    private static Color broom2 = new Color(217, 167, 98);
+    private static Color wizardHat = new Color(81, 0, 242);
+    private static Color stars = new Color(252, 255, 89);
+    private static Color orangeBg = new Color(232, 104, 0);
+    private static Color tree = new Color(24, 0, 48);
+    private static Color cinderBlocks = new Color(150, 150, 150);
+    private static Color branch = new Color(54, 23, 1);
+    private static Color moon1 = new Color(102, 0, 0);
+    private static Color moon2 = new Color(150, 0, 0);
+    private static Color moon3 = new Color(184, 28, 28);
+    private static Color lightOrange = new Color(224, 138, 67);
+    private static Color walloutline = new Color(90, 90, 90);
+    private static Color floor = new Color(120, 120, 120);
+    
     //class to make background
     public void bg1() {
-        c.setColor(tkit.orangeBg);
-        c.fillRect(0, 0, 640, 500);
-        
-        c.setColor(tkit.pumpkinStem);
-        int [] x = {650, 520, 480, 514, 650};
-        int [] y = {90, 70, 80, 50, 45};
-        
-        c.fillPolygon(x, y, 5);
+	c.setColor(orangeBg);
+	c.fillRect(0, 0, 640, 500);
+	
+	c.setColor(pumpkinStem);
+	int [] x = {650, 520, 480, 514, 650};
+	int [] y = {90, 70, 80, 50, 45};
+	
+	c.fillPolygon(x, y, 5);
     }
     
     public void bg2() {
-        c.setColor(tkit.orangeBg);
-        c.fillRect(0, 0, 650, 500);
-        
-        //sun
-        
-        c.setColor(tkit.moon3);
-        tkit.drawCircle(315, 270, 380);
-        c.setColor(tkit.sun); 
-        tkit.drawCircle(315, 270, 340);
-        
-        c.setColor(tkit.spider);
-        tkit.drawCircle(300, 690, 300);
-        c.setColor(Color.black);
-        c.drawOval(60, 390, 450, 300);
-        
-         //tree
-        c.setColor(Color.black);
-        for(int i=0; i<60; i++) {
-            c.drawLine(50, 340, 20+i, 420);
-        }
-        for(int i=0; i<20; i++) {
-            c.drawLine(40+i, 240, 40+i, 380);
-        }
-        for(int i=0; i<10; i++) {
-            c.drawLine(50+i, 240, 70, 200);
-            c.drawLine(50-i, 240, 30, 200);
-            
-            c.drawLine(60, 320+i, 80, 290+i);
-            c.drawLine(40, 300+i, 20, 270+i);
-            
-        }
-        for(int i=0; i<5; i++) {    
-            c.drawLine(62, 215-i, 55, 205-i);
-            c.drawLine(38, 215-i, 45, 205-i);
-            
-            c.drawLine(72, 310+i, 82, 306+i);
-            c.drawLine(30, 292-i, 24, 268-i);
-        }
-        
-        c.setColor(tkit.spider);
-        tkit.drawCircle(-20, 700, 300);
-        c.setColor(Color.black);
-        c.drawOval(-200, 400, 450, 250);
-        
-        c.setColor(tkit.spider);
-        tkit.drawCircle(560, 730, 350);
-        c.setColor(Color.black);
-        c.drawOval(260, 390, 800, 480);
+	c.setColor(orangeBg);
+	c.fillRect(0, 0, 650, 500);
+	
+	//sun
+	
+	c.setColor(moon3);
+	tkit.drawCircle(315, 270, 380);
+	c.setColor(sum); 
+	tkit.drawCircle(315, 270, 340);
+	
+	c.setColor(spider);
+	tkit.drawCircle(300, 690, 300);
+	c.setColor(Color.black);
+	c.drawOval(60, 390, 450, 300);
+	
+	 //tree
+	c.setColor(Color.black);
+	for(int i=0; i<60; i++) {
+	    c.drawLine(50, 340, 20+i, 420);
+	}
+	for(int i=0; i<20; i++) {
+	    c.drawLine(40+i, 240, 40+i, 380);
+	}
+	for(int i=0; i<10; i++) {
+	    c.drawLine(50+i, 240, 70, 200);
+	    c.drawLine(50-i, 240, 30, 200);
+	    
+	    c.drawLine(60, 320+i, 80, 290+i);
+	    c.drawLine(40, 300+i, 20, 270+i);
+	    
+	}
+	for(int i=0; i<5; i++) {    
+	    c.drawLine(62, 215-i, 55, 205-i);
+	    c.drawLine(38, 215-i, 45, 205-i);
+	    
+	    c.drawLine(72, 310+i, 82, 306+i);
+	    c.drawLine(30, 292-i, 24, 268-i);
+	}
+	
+	c.setColor(spider);
+	tkit.drawCircle(-20, 700, 300);
+	c.setColor(Color.black);
+	c.drawOval(-200, 400, 450, 250);
+	
+	c.setColor(spider);
+	tkit.drawCircle(560, 730, 350);
+	c.setColor(Color.black);
+	c.drawOval(260, 390, 800, 480);
     }
     
     public Background(Console con) {
-        c = con;
+	c = con;
     }
     public void moon() {
-        for (int i = 0; i < 123; i++) {
-            int red = (int) (102+ (i / 122.0) * 80.0);
-            int green = (int) ((i / 122.0) * 28.0);
-            int blue = (int) ((i / 122.0) * 28.0);
-            Color moonColor = new Color(red, green, blue);
-            c.setColor(moonColor);
-            c.drawOval(325 - i, 140 - i, 2*i, 2*i);
-        }
-        
+	for (int i = 0; i < 123; i++) {
+	    int red = (int) (102+ (i / 122.0) * 80.0);
+	    int green = (int) ((i / 122.0) * 28.0);
+	    int blue = (int) ((i / 122.0) * 28.0);
+	    Color moonColor = new Color(red, green, blue);
+	    c.setColor(moonColor);
+	    c.drawOval(325 - i, 140 - i, 2*i, 2*i);
+	}
+	
     }
     
     public void run() {
--- Spider.java
+++ Spider.java
@@ -12,61 +12,61 @@ import java.lang.*;     // to access Thread class
 public class Spider extends Thread {
     private Console c;
 
-    public void drawSpider (int x, int y) {
-        //x = 502, y=110
-        for(int i=0; i<80; i++) {
-            c.setColor(Color.white);
-            c.fillRect(x+13, 0, x-498, y+10);
-            
-            c.setColor(tkit.spider);
-            //body
-            c.fillArc(x, y, 30, 20, 0, 180);
-            c.fillRect(x, y+10, 30, 10);
-            c.fillArc(x, y+6, 30, 20, 180, 180);
-            //legs
-            c.drawLine(x+4, y+20, x, y+30);
-            c.drawLine(x, y+30, x-2, y+37);
-            c.drawLine(x+5, y+20, x+1, y+30);
-            c.drawLine(x+1, y+30, x-1, y+37);
-            
-            c.drawLine(x+24, y+20, x+28, y+30);
-            c.drawLine(x+28, y+30, x+28, y+37);
-            c.drawLine(x+25, y+20, x+29, y+30);
-            c.drawLine(x+29, y+30, x+29, y+37);
-            
-            c.drawLine(x+4, y+5, x-2, y-5);
-            c.drawLine(x-2, y-5, x+8, y-7);
-            c.drawLine(x+3, y+5, x-3, y-5);
-            c.drawLine(x-3, y-5, x+7, y-7);
-            
-            c.drawLine(x+24, y+5, x+30, y-5);
-            c.drawLine(x+30, y-5, x+20, y-7);
-            c.drawLine(x+25, y+4, x+31, y-5);
-            c.drawLine(x+31, y-5, x+21, y-7);
-            
-            c.drawLine(x, y+8, x-8, y-4);
-            c.drawLine(x-8, y-4, x+4, y-14);
-            c.drawLine(x-1, y+8, x-9, y-4);
-            c.drawLine(x-9, y-4, x+3, y-14);
-            
-            c.drawLine(x+28, y+8, x+36, y-4);
-            c.drawLine(x+36, y-4, x+24, y-14);
-            c.drawLine(x+29, y+8, x+37, y-4);
-            c.drawLine(x+37, y-4, x+25, y-14);
-            
-            c.setColor(tkit.orangeBg);
-            c.fillOval(x+6, y+10, 5, 5);
-            c.fillOval(x+20, y+10, 5, 5);
-        }
+    public void drawSpider (Console con, int x, int y) {
+	//x = 502, y=110
+	for(int i=0; i<80; i++) {
+	    c.setColor(Color.white);
+	    c.fillRect(x+13, 0, x-498, y+10);
+	    
+	    c.setColor(tkit.spider);
+	    //body
+	    c.fillArc(x, y, 30, 20, 0, 180);
+	    c.fillRect(x, y+10, 30, 10);
+	    c.fillArc(x, y+6, 30, 20, 180, 180);
+	    //legs
+	    c.drawLine(x+4, y+20, x, y+30);
+	    c.drawLine(x, y+30, x-2, y+37);
+	    c.drawLine(x+5, y+20, x+1, y+30);
+	    c.drawLine(x+1, y+30, x-1, y+37);
+	    
+	    c.drawLine(x+24, y+20, x+28, y+30);
+	    c.drawLine(x+28, y+30, x+28, y+37);
+	    c.drawLine(x+25, y+20, x+29, y+30);
+	    c.drawLine(x+29, y+30, x+29, y+37);
+	    
+	    c.drawLine(x+4, y+5, x-2, y-5);
+	    c.drawLine(x-2, y-5, x+8, y-7);
+	    c.drawLine(x+3, y+5, x-3, y-5);
+	    c.drawLine(x-3, y-5, x+7, y-7);
+	    
+	    c.drawLine(x+24, y+5, x+30, y-5);
+	    c.drawLine(x+30, y-5, x+20, y-7);
+	    c.drawLine(x+25, y+4, x+31, y-5);
+	    c.drawLine(x+31, y-5, x+21, y-7);
+	    
+	    c.drawLine(x, y+8, x-8, y-4);
+	    c.drawLine(x-8, y-4, x+4, y-14);
+	    c.drawLine(x-1, y+8, x-9, y-4);
+	    c.drawLine(x-9, y-4, x+3, y-14);
+	    
+	    c.drawLine(x+28, y+8, x+36, y-4);
+	    c.drawLine(x+36, y-4, x+24, y-14);
+	    c.drawLine(x+29, y+8, x+37, y-4);
+	    c.drawLine(x+37, y-4, x+25, y-14);
+	    
+	    c.setColor(tkit.orangeBg);
+	    c.fillOval(x+6, y+10, 5, 5);
+	    c.fillOval(x+20, y+10, 5, 5);
+	}
     }
 
 
     public Spider (Console con){
-        c = con;
+	c = con;
     }
 
 
     public void run () {
-        drawSpider(502, 110);
+	drawSpider(502, 110);
     }
 }
