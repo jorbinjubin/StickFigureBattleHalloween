18d7552e5e506c7657054bcdb482a9570ec2b9ae
Justin Jiang <142069933+jorbinjubin@users.noreply.github.com>
2023-11-10 21:52:11 -0500

    Update MyCreation.java

--- MyCreation.java
+++ MyCreation.java
@@ -2,66 +2,87 @@
 import hsa.Console;
 import java.awt.*;
 /*
- * Runs the animation by creating objects and running them. Uses threads to run 
+ * Runs the animation by creating objects and running them. Uses threads to run
  * multiple animated objects at the same time. Bubbles use an overloaded constructor
  * to create bubbles of different sizes and speeds.
- * 
+ *
  * Thanks to Nicholas Ng for the drawCircle code, my math abilities are
  * nowhere good enough to figure that out. - Justin
  *
-==========    ||     ||    ||======    		||=====
-    ||        ||     ||    ||          		||
-    ||        ||=====||    ||====      		|| 
+==========    ||     ||    ||======             ||=====
+    ||        ||     ||    ||                   ||
+    ||        ||=====||    ||====               ||
     ||        ||     ||    ||                   ||
     ||        ||     ||    ||======             ||======    onsole is 640 by 500 i dont wanna make the rest of the big letters lmfao
-    
-    
+
+
  * @author Justin Jiang, Fei Wang
  * ICS3UP
  * 2023/11/02
  */
 
 //try {Thread.sleep(ms);} catch (Exception e) {}
-public class MyCreation {
+public class MyCreation
+{
     static Console c; // The output console
-    public static int scene = 0; 
+    public static int scene = 0;
 
     // adds the Background thread to MyCreation
-    public void background() {
-	// not a thread because it's not animated!!!
-	Background b = new Background(c);
-	if(scene==0) {
-            b.bg1();
-            b.moon();
+    public void background ()
+    {
+        // not a thread because it's not animated!!!
+        Background b = new Background (c);
+        if (scene == 0)
+        {
+            b.bg1 ();
+            b.moon ();
         }
-        else if(scene==1) {
-            b.bg2();
+        else if (scene == 1)
+        {
+            b.bg2 ();
         }
-        else if(scene==2) {
-            b.bg3();
+        else if (scene == 2)
+        {
+            b.bg3 ();
         }
     }
 
-    public void splash() {  
-	Splash s = new Splash(c);
-	s.start();
-	try{s.join();} catch (InterruptedException e){}
+
+    public void splash ()
+    {
+        Splash s = new Splash (c);
+        s.start ();
+        try
+        {
+            s.join ();
+        }
+        catch (InterruptedException e)
+        {
+        }
     }
-    
-    public void spider() {
-	Spider sp = new Spider(c);
-	sp.run();
+
+
+    public void spider ()
+    {
+        Spider sp = new Spider (c);
+        sp.run ();
     }
-    
-    public void bat() {
-	Bat b = new Bat(c);
-	b.run();
+
+
+    public void bat ()
+    {
+        Bat b = new Bat (c);
+        b.run ();
     }
-    
-    public void ghost() {
-	Ghost g = new Ghost(c);
-	g.run();
+
+
+    public void ghost ()
+    {
+        Ghost g = new Ghost (c);
+        g.run ();
     }
+
+
     // adds the Lightning thread to MyCreation
     //  public void lightning ()
     //  {
@@ -71,7 +92,7 @@ public class MyCreation {
     //      j.start ();
     //
     //      //joins with SinkingBoat thread so that it only executes when
-    //      lightning thread is done 
+    //      lightning thread is done
     //      try
     //      {
     //          j.join ();
@@ -82,35 +103,45 @@ public class MyCreation {
     //  }
 
     // creates a new window and gives window a title
-    
-    public MyCreation() {
-	c = new Console("Stick Figure Battle");
+
+    public MyCreation ()
+    {
+        c = new Console ("Stick Figure Battle");
     }
-    public void sm1() {
-	Stickman1 sm = new Stickman1(c);
-	sm.run();
+
+
+    public void sm1 ()
+    {
+        Stickman1 sm = new Stickman1 (c);
+        sm.run ();
     }
-    
-    public static void main(String[] args) {
-	MyCreation z = new MyCreation();
-	z.background();
-	z.bat();
-	z.spider();
-	z.splash();
-	z.sm1();
-	c.getChar();
-	scene++;
-	z.background();
-	z.ghost();
-        z.bat();
-        z.spider();
-        z.splash();
-        c.getChar();
+
+
+    public static void main (String[] args)
+    {
+        MyCreation z = new MyCreation ();
+        z.background ();
+        z.bat ();
+        z.spider ();
+        z.splash ();
+        z.sm1 ();
+        c.getChar ();
         scene++;
-        z.background();
-        z.ghost();
-        c.getChar(); 
+
+        Background b = new Background (c);
+        b.bg2 ();
+        z.ghost ();
+        z.background ();
+        z.sm1();
+        z.bat ();
+        z.spider ();
+        c.getChar ();
         scene++;
-        z.background();
+
+        z.background ();
+        z.ghost ();
+        c.getChar ();
+        scene++;
+        z.background ();
     }
 }
