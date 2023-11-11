0dd2733861e3db001565a16dada2bcc76512b10a
Justin Jiang <142069933+jorbinjubin@users.noreply.github.com>
2023-11-10 21:14:12 -0500

    oopsie

--- Background.java
+++ Background.java
@@ -1,147 +1,128 @@
-// The "MyCreation" class.
-import hsa.Console;
 import java.awt.*;
+import hsa.Console;
+
 /*
- * Runs the animation by creating objects and running them. Uses threads to run
- * multiple animated objects at the same time. Bubbles use an overloaded constructor
- * to create bubbles of different sizes and speeds.
- *
- * Thanks to Nicholas Ng for the drawCircle code, my math abilities are
- * nowhere good enough to figure that out. - Justin
- *
-==========    ||     ||    ||======             ||=====
-    ||        ||     ||    ||                   ||
-    ||        ||=====||    ||====               ||
-    ||        ||     ||    ||                   ||
-    ||        ||     ||    ||======             ||======    onsole is 640 by 500 i dont wanna make the rest of the big letters lmfao
-
-
+ * Backgrounds for Stick Figure Battles.
  * @author Justin Jiang, Fei Wang
  * ICS3UP
- * 2023/11/02
+ * 2023/11/2
  */
 
-//try {Thread.sleep(ms);} catch (Exception e) {}
-public class MyCreation
-{
-    static Console c; // The output console
-    public static int scene = 0;
-
-    // adds the Background thread to MyCreation
-    public void background ()
-    {
-        // not a thread because it's not animated!!!
-        Background b = new Background (c);
-        if (scene == 0)
-        {
-            b.bg1 ();
-            b.moon ();
-        }
-        else if (scene == 1)
-        {
-            b.bg2 ();
-        }
-        else if (scene == 2)
-        {
-            b.bg3 ();
-        }
-    }
-
-
-    public void splash ()
-    {
-        Splash s = new Splash (c);
-        s.start ();
-        try
-        {
-            s.join ();
-        }
-        catch (InterruptedException e)
-        {
+public class Background {
+    //Declaring the local console variable
+    private Console c;
+    
+    
+    private  Color sun = new Color(208, 66, 14); 
+    private  Color pumpkin = new Color(255, 170, 10); 
+    private  Color pumpkinEyes = new Color(255, 219, 77); 
+    private  Color pumpkinStem = new Color(112, 58, 3);
+    private  Color lightPink = new Color(252, 159, 238);
+    private  Color mouse = new Color(212, 212, 212);
+    private  Color bat = new Color(58, 9, 96);
+    private  Color spider = new Color(74, 0, 0);
+    private  Color broom = new Color(237, 197, 128);
+    private  Color broom2 = new Color(217, 167, 98);
+    private  Color wizardHat = new Color(81, 0, 242);
+    private  Color stars = new Color(252, 255, 89);
+    private  Color orangeBg = new Color(232, 104, 0);
+    private  Color tree = new Color(24, 0, 48);
+    private  Color cinderBlocks = new Color(150, 150, 150);
+    private  Color branch = new Color(54, 23, 1);
+    private  Color moon1 = new Color(102, 0, 0);
+    private  Color moon2 = new Color(150, 0, 0);
+    private  Color moon3 = new Color(184, 28, 28);
+    private  Color lightOrange = new Color(224, 138, 67);
+    private  Color walloutline = new Color(90, 90, 90);
+    private  Color floor = new Color(120, 120, 120);
+    public Background(Console con) {
+        c = con;
+    }   
+    //method to draw a circle without using fill commands
+    public void drawCircle(Console c, int startX, int startY, int radius) {
+        for(int i=-radius; i<=radius; i++) {
+            c.drawLine(startX-(int)Math.sqrt(radius*radius-i*i), startY-i, startX+(int)Math.sqrt(radius*radius-i*i), startY-i);
         }
     }
-
-
-    public void spider ()
-    {
-        Spider sp = new Spider (c);
-        sp.run ();
+        
+    //class to make background
+    public void bg1() {
+        c.setColor(orangeBg);
+        c.fillRect(0, 0, 640, 500);
+        
+        c.setColor(pumpkinStem);
+        int [] x = {650, 520, 480, 514, 650};
+        int [] y = {90, 70, 80, 50, 45};
+        
+        c.fillPolygon(x, y, 5);
     }
-
-
-    public void bat ()
-    {
-        Bat b = new Bat (c);
-        b.run ();
+    
+    public void bg2() {
+        c.setColor(orangeBg);
+        c.fillRect(0, 0, 650, 500);
+        
+        //sun
+        
+        c.setColor(moon3);
+        drawCircle(c, 315, 270, 380);
+        c.setColor(sun); 
+        // drawCircle(c, 315,0, 340);
+        
+        c.setColor(spider);
+        drawCircle(c, 300, 690, 300);
+        // c.setColor(Color.black);
+        // c.drawOval(60, 390, 450, 300);
+        
+         //tree
+        c.setColor(Color.black);
+        for(int i=0; i<60; i++) {
+            c.drawLine(50, 340, 20+i, 420);
+        }
+        for(int i=0; i<20; i++) {
+            c.drawLine(40+i, 240, 40+i, 380);
+        }
+        for(int i=0; i<10; i++) {
+            c.drawLine(50+i, 240, 70, 200);
+            c.drawLine(50-i, 240, 30, 200);
+            
+            c.drawLine(60, 320+i, 80, 290+i);
+            c.drawLine(40, 300+i, 20, 270+i);
+            
+        } 
+        for(int i=0; i<5; i++) {    
+            c.drawLine(62, 215-i, 55, 205-i);
+            c.drawLine(38, 215-i, 45, 205-i);
+            
+            c.drawLine(72, 310+i, 82, 306+i);
+            c.drawLine(30, 292-i, 24, 268-i);
+        }
+        
+        c.setColor(spider);
+        drawCircle(c, -20, 700, 300);
+        // c.setColor(Color.black);
+        // c.drawOval(-200, 400, 450, 250);
+        
+        c.setColor(spider);
+        drawCircle(c, 560, 730, 350);
+        // c.setColor(Color.black);
+        // c.drawOval(260, 390, 800, 480);
     }
-
-
-    public void ghost ()
-    {
-        Ghost g = new Ghost (c);
-        g.run ();
+    
+    
+    public void moon() {
+        for (int i = 0; i < 123; i++) {
+            int red = (int) (102+ (i / 122.0) * 80.0);
+            int green = (int) ((i / 122.0) * 28.0);
+            int blue = (int) ((i / 122.0) * 28.0);
+            Color moonColor = new Color(red, green, blue);
+            c.setColor(moonColor);
+            c.drawOval(325 - i, 140 - i, 2*i, 2*i);
+        }
+        
     }
-
-
-    // adds the Lightning thread to MyCreation
-    //  public void lightning ()
-    //  {
-    //      //creates the thread
-    //      Lightning j = new Lightning (c);
-    //      //starts the thread
-    //      j.start ();
-    //
-    //      //joins with SinkingBoat thread so that it only executes when
-    //      lightning thread is done
-    //      try
-    //      {
-    //          j.join ();
-    //      }
-    //      catch (InterruptedException e)
-    //      {
-    //      }
-    //  }
-
-    // creates a new window and gives window a title
-
-    public MyCreation ()
-    {
-        c = new Console ("Stick Figure Battle");
-    }
-
-
-    public void sm1 ()
-    {
-        Stickman1 sm = new Stickman1 (c);
-        sm.run ();
-    }
-
-
-    public static void main (String[] args)
-    {
-        MyCreation z = new MyCreation ();
-        z.background ();
-        z.bat ();
-        z.spider ();
-        z.splash ();
-        z.sm1 ();
-        c.getChar ();
-        scene++;
-
-        Background b = new Background (c);
-        b.bg2 ();
-        z.ghost ();
-        z.background ();
-        z.sm1();
-        z.bat ();
-        z.spider ();
-        c.getChar ();
-        scene++;
-
-        z.background ();
-        z.ghost ();
-        c.getChar ();
-        scene++;
-        z.background ();
-    }
+    
+    public void run() {
+       bg1();
+       moon();
+    } 
 }
