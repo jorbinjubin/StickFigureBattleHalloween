79bc49cbee625c5a6f9fb9fb84b2fd3c5cdb097e
Insistent Bean <109440858+TszFeiW@users.noreply.github.com>
2023-11-13 13:03:34 -0500

    Update MyCreation.java

--- MyCreation.java
+++ MyCreation.java
@@ -6,8 +6,7 @@ import java.awt.*;
  * multiple animated objects at the same time. Bubbles use an overloaded constructor
  * to create bubbles of different sizes and speeds.
  *
- * Thanks to Nicholas Ng for the drawCircle code, my math abilities are
- * nowhere good enough to figure that out. - Justin
+ * Thanks to Nicholas Ng for the drawCircle code
  *
 ==========    ||     ||    ||======             ||=====
     ||        ||     ||    ||                   ||
@@ -26,7 +25,9 @@ public class MyCreation
 {
     static Console c; // The output console
     public static int scene = 0;
-
+    private Color potion = new Color(147, 4, 224);
+    private Color moon2 = new Color(150, 0, 0);
+    
     // adds the Background thread to MyCreation
     public void background ()
     {
@@ -46,51 +47,22 @@ public class MyCreation
             b.bg3 ();
         }
     }
-
-
+    
+    public void ghost ()
+    {
+        Ghost g = new Ghost (c);
+        g.run ();
+    }
+    
     public void splash ()
     {
         Splash s = new Splash (c);
         s.start ();
-        try
-        {
+        try {    
             s.join ();
         }
-        catch (InterruptedException e)
-        {
-        }
+        catch (InterruptedException e) {}
     }
-
-
-    public void spider ()
-    {
-        Spider sp = new Spider (c);
-        if(scene == 0) {
-            for(int y = 0; y<25; y++) {
-                sp.spiderCrawl(502, 110-y, true, 0);
-                try {Thread.sleep(50);} catch (Exception e) {}
-                sp.spiderCrawl(502, 110-y, false, 0);
-            }
-        
-            for(int n = 0; n<42; n++) {
-                sp.drawSpider(502+(6*n), 92+(n/2), true);
-                try {Thread.sleep(50);} catch (Exception e) {}
-                sp.drawSpider(502+(6*n), 92+(n/2), false);
-            }
-        }
-        else if(scene == 2) {
-            for(int y = 0; y<25; y++) {
-                sp.spiderCrawl(582, 60-y, true, 2);
-                try {Thread.sleep(50);} catch (Exception e) {}
-                sp.spiderCrawl(582, 60-y, false, 2);
-            }
-            sp.spiderCrawl(582, 60-25, true, 2);
-        }
-        sp.start();
-        try {sp.join();}
-        catch (InterruptedException e){}
-    }
-
 
     public void bat ()
     {
@@ -106,10 +78,10 @@ public class MyCreation
                 catch(Exception e) {}
                 b.drawBat(batX, batY, false, 0);
             }
-            b.run();
+            b.start();
         }  
         
-        Bat b2 = new Bat(c, Bubbles.potion);
+        Bat b2 = new Bat(c, potion);
         if(scene == 2) {
             int x = 100;
             int y = 40; 
@@ -135,60 +107,80 @@ public class MyCreation
             b2.start();
             try {b2.join();}
             catch (InterruptedException e){}
-        }
-    }
+        } 
 
+    }
     
-    public void ghost ()
+    public void spider ()
     {
-        Ghost g = new Ghost (c);
-        g.run ();
+        Spider sp = new Spider (c);
+        if(scene == 0) {
+            for(int y = 0; y<25; y++) {
+                sp.spiderCrawl(502, 110-y, true, 0);
+                try {Thread.sleep(50);} catch (Exception e) {}
+                sp.spiderCrawl(502, 110-y, false, 0);
+            }
+        
+            for(int n = 0; n<42; n++) {
+                sp.drawSpider(502+(6*n), 92+(n/2), true);
+                try {Thread.sleep(50);} catch (Exception e) {}
+                sp.drawSpider(502+(6*n), 92+(n/2), false);
+            }
+            sp.start();
+        }
+        Spider sp2 = new Spider(c, moon2);
+        if(scene == 2) {
+            for(int y = 0; y<25; y++) {
+                sp2.spiderCrawl(582, 60-y, true, 2);
+                try {Thread.sleep(50);} catch (Exception e) {}
+                sp2.spiderCrawl(582, 60-y, false, 2);
+            }
+            sp2.spiderCrawl(582, 60-25, true, 2);
+        }
+        sp2.start();
     }
 
-    public MyCreation ()
-    {
-        // creates a new window and gives window a title
-        c = new Console ("Stick Figure Battle");
-    }
-
-
-    public void sm1 () {
-        Stickman1 sm = new Stickman1 (c);
-        sm.start ();
-    } 
-    public void sm2() {
-        Stickman2 sm2 = new Stickman2(c);
-        Thread sm2_t = new Thread(sm2);
-        sm2_t.start();
-    }
     public void bubbles() {
         Bubbles bub = new Bubbles(c);
         bub.start();   
     }
 
+    public MyCreation ()
+    {
+        c = new Console ("Stick Figure Battle");
+    }
+
+
+    public void sm1 ()
+    {
+        Stickman1 sm = new Stickman1 (c);
+        sm.run ();
+    } 
+
+
     public static void main (String[] args)
     {
         MyCreation z = new MyCreation ();
         z.background ();
+        z.bat ();
+        z.spider ();
         z.splash ();
         z.sm1 ();
         c.getChar ();
-        z.bat ();
-        z.spider ();
-        scene++;
         
+        scene++;
         c.clear();
         z.background ();
         z.ghost ();
         z.sm1();
-        z.sm2();
-        
         c.getChar ();
+        
         scene++;
         c.clear();
         z.background ();
         z.bubbles();
         z.bat();
         z.spider();
+        
     }
 }
